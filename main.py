f15792b9a978f743c521518163987c27e0dbfc76
Ayushx13 <ayushpofficialx@gmail.com>
2026-03-04 02:54:46 +0530

    path and decorator

--- main.py
+++ main.py
@@ -2,7 +2,35 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"message":"Hello world!"}
 
+# @app -> path operation decorator , get -> http method or Operation , "/" -> path
+@app.get("/")
+def index(): #path operation function
+    return {
+        "data": "Blog List"
+    }
+
+
+@app.get("/blog/unpublished")
+def unpublished_blogs():
+    return {
+        "data": "All unpublished blogs"
+    }
+
+
+@app.get("/blog/{id}")
+def get_blog(id: int):
+    return {
+        #"data": f"Blog with id {id}"
+        "data": "Blog with id " + str(id)
+    }
+
+
+@app.get("/blog/{id}/comments")
+def get_blog_comments(id: int):
+    return {
+        "data": {
+            "1": "This is the first comment",
+            "2": "This is the second comment"
+        }
+    }
